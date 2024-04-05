df3242a92514cd668df52b04157cf07b064d7c63
Juan Saenz Valiente <j2saenzvaliente@gmail.com>
2024-04-05 13:17:23 -0300

    migrar_cables_ipt

--- migrar_cables_ipt.py
+++ migrar_cables_ipt.py
@@ -14,13 +14,39 @@ migrador = MigrarInfo(company_id='30',
                                     '@tramo'])
 lector.start(ruta_kmz)
 id_objeto = 528600
-fo_net = '31'
-infra_net = '30'
-patrones_estilo = ['inline', 'msn_ylw-pushpin', '#route', 'geocode']
-diccionario_sitios = {}
+fo_net = '2'
+infra_net = '4'
+
 
 def obtener_cantidad(hilos):
-    pass
+    hilos = str(hilos)
+    if hilos == '4':
+        return '4', '0'
+    elif hilos == '6':
+        return '6','0'
+    elif hilos == '8':
+        return '8','0'
+    elif hilos == '12':
+        return '12','0'
+    elif hilos == '24':
+        return '24x12','1'
+    elif hilos == '32':
+        return '32','4'
+    elif hilos == '36':
+        return '36','7'
+    elif hilos == '48':
+        return '48','2'
+    elif hilos == '72':
+        return '72','3'
+    elif hilos == '96':
+        return '96', '4'
+    elif hilos == '144':
+        return '144', '5'
+    elif hilos == '288':
+        return '288','6'
+    else:
+        print(hilos)
+    
 
 for i in lector.dict_objetos.values():
     nombre = i['Nombre']
@@ -28,8 +54,17 @@ for i in lector.dict_objetos.values():
     estilo = i['Estilo']
     coordenadas = i['Coordenadas']
     lista = i['Cuadro']
+    lista = lista[0]
     cantidad_hilos = lista['num_hilo']
     longitud = lista['longitud']
+    try:
+        localidad = lista['localidad']
+    except KeyError:
+        localidad = 'Sin Dato'
+    try:
+        inicio = lista['fo_inicio']
+    except KeyError:
+        inicio = 'Sin Dato'
 
     cantidad_hilos, buffers = obtener_cantidad(cantidad_hilos)
     tramo = nombre
@@ -41,7 +76,11 @@ for i in lector.dict_objetos.values():
                             '@mediciones': 'true',
                             '@capacidad':cantidad_hilos,
                             '@buffers':buffers,
-                            "@estado":"E",
+                            '@longitud':longitud,
+                            '@inicio': inicio,
+                            '@estado':'E',
+                            '@localidad':localidad.capitalize()
+
                         },
                         nID=fo_net)
     id_objeto += 1  # Incrementar el ID del objeto para la siguiente iteración
