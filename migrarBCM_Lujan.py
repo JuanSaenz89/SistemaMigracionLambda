b8420ff50095f28a80544a517219bf3aca7ada17
Juan Saenz Valiente <j2saenzvaliente@gmail.com>
2025-02-26 11:18:58 -0300

    Convertido a Objeto

--- migrarBCM_Lujan.py
+++ migrarBCM_Lujan.py
@@ -28,12 +28,12 @@ class MigradorBCM():
         self.ClientsDict = {}
     
     def start(self):
-        self.migrateCierres(self.ruta_excel)
+        self.migrateCierres(self.ruta_excel, 'Listado por Origen')
 
 
-    def migrateCierres(self, filename):
+    def migrateCierres(self, filename, pageName):
         wb = load_workbook(filename = filename)
-        hoja = wb['Listado por Origen']
+        hoja = wb[pageName]
 
         for i in range(5,hoja.max_row + 1):
             id = hoja.cell(row=i, column=1).value
@@ -61,11 +61,11 @@ class MigradorBCM():
                     '@oType': '2N',
                     '@ref': direccion
                 }
-                migrador.crear_objeto(id=objectID,
+                self.migrador.crear_objeto(id=objectID,
                                 oType='go/fo/cie',
                                 vectores=coordenadas,
                                 vals=vals,
-                                nID=fo_net)
+                                nID=self.fo_net)
                 objectID += 1  # Incrementar el ID del objeto para la siguiente iteración
             elif tipo == 'FO: Sangrado / FDH':
 
@@ -73,10 +73,10 @@ class MigradorBCM():
                     equipamiento = equipamiento.split('_')
                     nombre = equipamiento[1]
 
-                if nombre not in nameList:
-                    nameList.append(nombre)
+                if nombre not in self.nameList:
+                    self.nameList.append(nombre)
 
-                elif nombre in nameList:
+                elif nombre in self.nameList:
                     print(nombre)
                     continue
 
@@ -85,68 +85,71 @@ class MigradorBCM():
                     '@oType': '1N',
                     '@ref': direccion
                 }
-                migrador.crear_objeto(id=objectID,
+                self.migrador.crear_objeto(id=objectID,
                                 oType='go/fo/cie',
                                 vectores=coordenadas,
                                 vals=vals,
-                                nID=fo_net)
+                                nID=self.fo_net)
                 objectID += 1  # Incrementar el ID del objeto para la siguiente iteración
-        
-    wb = load_workbook(filename=ruta_excel_clientes)
-    hoja = wb['Red Por Cliente']
-    unixtime = "1..1."
-    for i in range(5,hoja.max_row + 1):
-        numero = hoja.cell(row=i, column=1).value
-        nombre = hoja.cell(row=i, column=2).value
-        domicilio = hoja.cell(row=i, column=3).value
-        latitud = hoja.cell(row=i, column=4).value
-        longitud = hoja.cell(row=i, column=5).value
-        plan = hoja.cell(row=i, column=6).value
-        bloqueo = hoja.cell(row=i, column=7).value
-        usuario = hoja.cell(row=i, column=8).value
-        serialNumber = hoja.cell(row=i, column=9).value
-        ontId = hoja.cell(row=i, column=10).value
-        ip = hoja.cell(row=i, column=11).value
-        mac = hoja.cell(row=i, column=12).value
-        cableModem = hoja.cell(row=i, column=13).value
-        ssid = hoja.cell(row=i, column=14).value
-        claveSsid = hoja.cell(row=i, column=15).value
-        ssid5 = hoja.cell(row=i, column=16).value
-        claveSsid5 = hoja.cell(row=i, column=17).value
-        padreDirecto = hoja.cell(row=i, column=19).value
-        padre = hoja.cell(row=i, column=19).value
-        com = hoja.cell(row=i, column=20).value
 
-        if not latitud or not longitud:
-            continue
-        coordenadas = [[[longitud , latitud]]]
 
-        vals = {
-            '@oName': numero,
-            '@nombre': nombre,
-            '@domicilio': domicilio,
-            '@plan': plan,
-            '@bloqueo': bloqueo,
-            '@usuario': usuario,
-            '@serialNumber': serialNumber,
-            '@ontId': ontId,
-            '@ip': ip,
-            '@mac': mac,
-            '@cableModem': cableModem,
-            '@ssid': ssid,
-            '@claveSsid': claveSsid,
-            '@ssid5': ssid5,
-            '@claveSsid5': claveSsid5,
-            '@padreDirecto': padreDirecto,
-            '@padre': padre,
-            '@com': com,
-        }
-        # Filtrar las claves con valores no None
-        vals_filtrados = {k: v for k, v in vals.items() if v is not None}
 
-        migrador.crear_objeto(id=objectID,
-                            oType='go/fo/cli',
-                            vectores=coordenadas,
-                            vals=vals_filtrados,
-                            nID=clientNetID)
-        self.objectID += 1  # Incrementar el ID del objeto para la siguiente iteración    
+    def migrateClientes(self, filename):
+        wb = load_workbook(filename=filename)
+        hoja = wb['Red Por Cliente']
+        unixtime = "1..1."
+        for i in range(5,hoja.max_row + 1):
+            numero = hoja.cell(row=i, column=1).value
+            nombre = hoja.cell(row=i, column=2).value
+            domicilio = hoja.cell(row=i, column=3).value
+            latitud = hoja.cell(row=i, column=4).value
+            longitud = hoja.cell(row=i, column=5).value
+            plan = hoja.cell(row=i, column=6).value
+            bloqueo = hoja.cell(row=i, column=7).value
+            usuario = hoja.cell(row=i, column=8).value
+            serialNumber = hoja.cell(row=i, column=9).value
+            ontId = hoja.cell(row=i, column=10).value
+            ip = hoja.cell(row=i, column=11).value
+            mac = hoja.cell(row=i, column=12).value
+            cableModem = hoja.cell(row=i, column=13).value
+            ssid = hoja.cell(row=i, column=14).value
+            claveSsid = hoja.cell(row=i, column=15).value
+            ssid5 = hoja.cell(row=i, column=16).value
+            claveSsid5 = hoja.cell(row=i, column=17).value
+            padreDirecto = hoja.cell(row=i, column=19).value
+            padre = hoja.cell(row=i, column=19).value
+            com = hoja.cell(row=i, column=20).value
+
+            if not latitud or not longitud:
+                continue
+            coordenadas = [[[longitud , latitud]]]
+
+            vals = {
+                '@oName': numero,
+                '@nombre': nombre,
+                '@domicilio': domicilio,
+                '@plan': plan,
+                '@bloqueo': bloqueo,
+                '@usuario': usuario,
+                '@serialNumber': serialNumber,
+                '@ontId': ontId,
+                '@ip': ip,
+                '@mac': mac,
+                '@cableModem': cableModem,
+                '@ssid': ssid,
+                '@claveSsid': claveSsid,
+                '@ssid5': ssid5,
+                '@claveSsid5': claveSsid5,
+                '@padreDirecto': padreDirecto,
+                '@padre': padre,
+                '@com': com,
+            }
+            # Filtrar las claves con valores no None
+            vals_filtrados = {k: v for k, v in vals.items() if v is not None}
+
+            self.migrador.crear_objeto(id=self.objectID,
+                                oType='go/fo/cli',
+                                vectores=coordenadas,
+                                vals=vals_filtrados,
+                                nID=self.clientNetID)
+            self.objectID += 1  # Incrementar el ID del objeto para la siguiente iteración    
